2b78cb24c164347ee649f1e1c35a7f06d18e46bd
muertos <sirnickthebold@gmail.com>
2022-04-15 19:08:28 -0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-ansible-galaxy collection install -r requirements.yml
